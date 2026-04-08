c2cb711a457e33527c72b647705363fe80abe320
MWXGOD <1491377079@qq.com>
2026-04-08 15:17:58 +0800

    delete some py file

--- snap_download.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-
-
-# snapshot_download(repo_id="Qwen/Qwen-Audio", local_dir = "cache/Qwen-Audio", ignore_patterns=["tf_model.h5", "flax_model.msgpack", "*.safetensors"], resume_download=True)
-
-snapshot_download(repo_id="Qwen/Qwen2-Audio-7B", local_dir = "cache/Qwen2-Audio-7B", ignore_patterns=["tf_model.h5", "flax_model.msgpack", "*.safetensors"], resume_download=True)
